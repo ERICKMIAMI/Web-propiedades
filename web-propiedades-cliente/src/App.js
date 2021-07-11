8bf62f23a5931690bd32e86387d471a93be33846
ERICKMIAMI <kapacho.alejo51@gmail.com>
2021-07-11 00:20:27 -0600

    se agrega el proyecto

---
+++ web-propiedades-cliente/src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        WEB DE PROPIEDADES EN VENTA Y ALQUILERES
+      </h1>
+
+    </div>
+  );
+}
+
+export default App;
